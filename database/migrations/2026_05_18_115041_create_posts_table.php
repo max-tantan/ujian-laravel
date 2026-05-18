7aa7193b247722bec3b9790067dbe94787979290
max-tantan <fatanjamterbang@gmail.com>
2026-05-18 18:57:02 +0700

    update posts migration to include user_id, title, and content fields

--- database/migrations/2026_05_18_115041_create_posts_table.php
+++ database/migrations/2026_05_18_115041_create_posts_table.php
@@ -6,22 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   public function up(): variant_mod
+   {
+    Schema::create('posts', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->string('title');
+        $table->text('content');
+        $table->timestamps();
+    });
+   }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('posts');
-    }
 };
