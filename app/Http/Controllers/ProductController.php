3dbd8f9b536be7c9fd291e9d7d97ca48a0d3a620
max-tantan <fatanjamterbang@gmail.com>
2026-05-18 13:29:01 +0700

    update gede

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,10 +10,36 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::all();
+
             return response()->json([
                 'success' => "true",
                 'message' => "List Data Products",
                 'data' => $products
             ], 200);
+    }
+    public function show(Product $products)
+    {
+        return response()->json([
+            'success' => "true",
+            'message' => "Detail Data Product",
+            'data' => $products
+        ], 200);
+    }
+    public function store(Request $request)
+    {
+        //validasi data
+        $request->validate([
+            'name' => 'required|max:25',
+            'price' => 'required|integer',
+            'description' => 'required|max:255',
+        ]);
+        //ambil input
+        $product = Product::create($request->all());
+        //simpan ke database
+        return response()->json([
+            'success' => "true",
+            'message' => "Product created successfully",
+            'data' => $product
+        ], 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,6 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/products', [ProductController::class, 'index']);
-
+Route::apiResource('products', ProductController::class);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
