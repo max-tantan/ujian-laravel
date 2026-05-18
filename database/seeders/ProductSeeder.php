27acad11c7f8f72ac51298b5e65591d550af63ea
max-tantan <fatanjamterbang@gmail.com>
2026-05-18 11:03:16 +0700

    seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,16 +2,29 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Product::create([
+            'name' => 'Laptop',
+            'price' => 15000000,
+            'description' => 'Laptop gaming dengan spesifikasi tinggi',
+        ]);
+
+        Product::create([
+            'name' => 'Smartphone',
+            'price' => 5000000,
+            'description' => 'Smartphone dengan kamera berkualitas tinggi',
+        ]);
+
+        Product::create([
+            'name' => 'Headphones',
+            'price' => 2000000,
+            'description' => 'Headphones dengan suara jernih dan nyaman digunakan',
+        ]);
     }
 }
